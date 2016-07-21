0b63c82153812a27807d6080591191922b42b2ec
fess <fess89@gmail.com>
2016-07-21 17:35:12 +0300

    Added model that makes use of Observables

--- app/src/main/java/com/omkarmoghe/pokemap/rx/api/APIMethods.java
+++ app/src/main/java/com/omkarmoghe/pokemap/rx/api/APIMethods.java
@@ -1,7 +1,34 @@
 package com.omkarmoghe.pokemap.rx.api;
 
+import retrofit2.Response;
+import retrofit2.http.Field;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import rx.Observable;
+
 /**
- * Created by fess on 21.07.16.
+ * API methods.
+ * <p>
+ * Created by fess on 20.07.16.
  */
-public class APIMethods {
+public interface APIMethods {
+
+    @GET("/login?service=https://sso.pokemon.com/sso/oauth2.0/callbackAuthorize")
+    Observable<Response> initialCall(@Header("User-Agent") String userAgent);
+
+    @GET("/login?service=https://sso.pokemon.com/sso/oauth2.0/callbackAuthorize")
+    Observable<Response> loginCall(@Field("lt") String lt,
+                                   @Field("execution") String execution,
+                                   @Field("_eventId") String eventId,
+                                   @Field("username") String username,
+                                   @Field("password") String password,
+                                   @Header("User-Agent") String userAgent);
+
+    @GET("/oauth2.0/accessToken")
+    Observable<Response> tokenCall(@Field("client_id") String clientId,
+                                   @Field("redirect_url") String redirectUrl,
+                                   @Field("client_secret") String clientSecret,
+                                   @Field("grant_type") String grantType,
+                                   @Field("code") String ticket,
+                                   @Header("User-Agent") String userAgent);
 }
--- app/src/main/java/com/omkarmoghe/pokemap/rx/api/APIUtils.java
+++ app/src/main/java/com/omkarmoghe/pokemap/rx/api/APIUtils.java
@@ -11,6 +11,7 @@ public class APIUtils {
     public static final String PTC_CLIENT_SECRET = "w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR";
     public static final String CLIENT_ID = "mobile-app_pokemon-go";
     public static final String REDIRECT_URI = "https://www.nianticlabs.com/pokemongo/error";
+    public static final String NIANTIC_APP = "Niantic App";
 
 }
 
