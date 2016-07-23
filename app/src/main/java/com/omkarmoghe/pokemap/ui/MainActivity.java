9b7e601439426e2d8913cfa2efb12bca50165512
Chris <chris.beda@gmail.com>
2016-07-23 00:46:46 -0700

    Added passing the Auth Token from login to the Main Activity. It handles saving it between save states. The activity takes the Auth Token and creates a AuthInfo object for use with the PokeGoAPI..

--- app/src/main/java/com/omkarmoghe/pokemap/ui/MainActivity.java
+++ app/src/main/java/com/omkarmoghe/pokemap/ui/MainActivity.java
@@ -1,11 +1,14 @@
 package com.omkarmoghe.pokemap.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -19,15 +22,51 @@ import com.omkarmoghe.pokemap.common.BaseActivity;
 import com.omkarmoghe.pokemap.map.MapWrapperFragment;
 import com.omkarmoghe.pokemap.settings.SettingsActivity;
 
+import java.lang.annotation.Retention;
+
+import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
 public class MainActivity extends BaseActivity implements OnMapReadyCallback{
 
     public static final String TAG = "Pokemap";
 
+    private static final String ARG_TOKEN = "Auth Token";
+    private static final String ARG_PROVIDER = "Token Provider";
+
+    public static final String PROVIDER_GOOGLE = "google";
+    public static final String PROVIDER_PTC = "PTC";
+
+    @Retention(SOURCE)
+    @StringDef({
+            PROVIDER_GOOGLE,
+            PROVIDER_PTC
+    })
+    @interface AuthProviders{}
+
     // fragments
     private MapWrapperFragment mMapWrapperFragment;
+    private AuthInfo mAuthInfo;
+    private String mAuthToken;
+    @AuthProviders
+    private String mAuthProvider;
 
     // Preferences
     SharedPreferences pref;
+
+    public static void start(Activity caller){
+        Intent intent = new Intent(caller, MainActivity.class);
+        caller.startActivity(intent);
+    }
+
+    public static void start(LoginActivity caller, String token, @AuthProviders String provider){
+        Intent intent = new Intent(caller, MainActivity.class);
+        intent.putExtra(ARG_TOKEN, token);
+        intent.putExtra(ARG_PROVIDER, provider);
+        caller.startActivity(intent);
+        caller.finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +74,30 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback{
         setContentView(R.layout.activity_main);
 
         pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        Intent intent = getIntent();
+        if(intent.hasExtra(ARG_TOKEN) && intent.hasExtra(ARG_PROVIDER)){
+            if(intent.getStringExtra(ARG_PROVIDER).equals(PROVIDER_GOOGLE)){
+                mAuthProvider = PROVIDER_GOOGLE;
+            }else{
+                mAuthProvider = PROVIDER_PTC;
+            }
+
+            mAuthToken = intent.getStringExtra(ARG_TOKEN);
+
+            mAuthInfo = createAuthInfo(mAuthToken, mAuthProvider);
+        }else if(savedInstanceState.containsKey(ARG_TOKEN) && savedInstanceState.containsKey(ARG_PROVIDER)){
+            if(savedInstanceState.getString(ARG_PROVIDER).equals(PROVIDER_GOOGLE)){
+                mAuthProvider = PROVIDER_GOOGLE;
+            }else{
+                mAuthProvider = PROVIDER_PTC;
+            }
+
+            mAuthToken = savedInstanceState.getString(ARG_TOKEN);
+
+            mAuthInfo = createAuthInfo(mAuthToken, mAuthProvider);
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -44,9 +107,22 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback{
         transaction.replace(R.id.main_container, mMapWrapperFragment)
                    .addToBackStack(null)
                    .commit();
-        login();
+
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        outState.putString(ARG_TOKEN, mAuthToken);
+        outState.putString(ARG_PROVIDER, mAuthProvider);
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(ARG_TOKEN, mAuthToken);
+        outState.putString(ARG_PROVIDER, mAuthProvider);
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,23 +135,10 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback{
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
-        } else if (id == R.id.action_relogin) {
-            login();
         }
 
         return super.onOptionsItemSelected(item);
     }
-
-
-    private void login() {
-
-        String username = pref.getString(getString(R.string.pref_username), "");
-        String password = pref.getString(getString(R.string.pref_password), "");
-
-        Log.d(TAG, "Username: " + username);
-        nianticManager.login(username, password, this);
-    }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -102,5 +165,13 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback{
                 break;
         }
         //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private static AuthInfo createAuthInfo(String token, @AuthProviders String provider){
+        AuthInfo.Builder authbuilder = AuthInfo.newBuilder();
+        authbuilder.setProvider(provider);
+        authbuilder.setToken(AuthInfo.JWT.newBuilder().setContents(token).setUnknown2(59).build());
+
+        return authbuilder.build();
     }
 }
