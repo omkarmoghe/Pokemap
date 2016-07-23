312a9a683d5020fd26fbd1d4c94784bad451f87b
Christopher Beda <chris.beda@gmail.com>
2016-07-23 13:27:33 -0700

    Login Screen and Google Authentication  (#70)
    * Added a ui folder and moved MainActivity to that folder. Created LoginActivity
    
    * Created a GoogleManager and GoogleService which handles the authentication done by Google
    
    * Added Google Auth Activity as a work around for the 1 button sign in for authenticating the google account
    
    * Cleaned up the Android Manifest. Added Login Activity and Google Auth Activity to the Manifest.
    
    * Added Strings for the Login Page
    
    * Added Cardview to the dependencies
    
    * Extracted the Toolbar in activity_main to Toolbar.xml
    
    * Added a Toolbar to the GoogleAuthActivity and a back arrow
    
    * Cleaned up the build.gradle dependencies
    
    * Added Warning to the Login Activity
    
    * Removed textview ids since they aren't used
    
    * Fixed PTC login and moved it over to Retrofit
    
    * Warning now disappears when logging in and added a ban warning to the warning text. Also fixed a crash when trying to login with google due to a second toolbar
    
    * Added passing the Auth Token from login to the Main Activity. It handles saving it between save states. The activity takes the Auth Token and creates a AuthInfo object for use with the PokeGoAPI..
    
    * Fixed a potential memory leak
    
    * Moved Main Activity to a ui folder
    
    * Fixed potential memory leak
    
    * Fixed a merge gone wrong
    
    * Fixed NetworkRequestLoggingInterceptor name change
    
    * Merged in latest changes
    
    * Revert one change
    
    * Fixed missing call to start the main activity

--- app/src/main/java/com/omkarmoghe/pokemap/controllers/net/NianticManager.java
+++ app/src/main/java/com/omkarmoghe/pokemap/controllers/net/NianticManager.java
@@ -2,16 +2,18 @@ package com.omkarmoghe.pokemap.controllers.net;
 
 import android.os.HandlerThread;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.omkarmoghe.pokemap.models.events.CatchablePokemonEvent;
-import com.omkarmoghe.pokemap.models.events.IEvent;
+
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.omkarmoghe.pokemap.models.events.LoginEventResult;
 import com.omkarmoghe.pokemap.models.events.ServerUnreachableEvent;
 import com.omkarmoghe.pokemap.models.events.TokenExpiredEvent;
 import com.pokegoapi.api.PokemonGo;
-import com.pokegoapi.api.map.pokemon.CatchablePokemon;
 import com.pokegoapi.auth.GoogleLogin;
 import com.pokegoapi.auth.PtcLogin;
 import com.pokegoapi.exceptions.LoginFailedException;
@@ -19,33 +21,54 @@ import com.pokegoapi.exceptions.RemoteServerException;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLSession;
+import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo;
 
-import POGOProtos.Networking.Envelopes.RequestEnvelopeOuterClass;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.pokegoapi.auth.PtcLogin.CLIENT_ID;
+import static com.pokegoapi.auth.PtcLogin.CLIENT_SECRET;
+import static com.pokegoapi.auth.PtcLogin.LOGIN_OAUTH;
+import static com.pokegoapi.auth.PtcLogin.LOGIN_URL;
+import static com.pokegoapi.auth.PtcLogin.REDIRECT_URI;
 
 /**
  * Created by vanshilshah on 20/07/16.
  */
 public class NianticManager {
+    private static final String TAG = "NianticManager";
+
+    private static final String BASE_URL = "https://sso.pokemon.com/sso/";
 
     private static final NianticManager instance = new NianticManager();
 
     private Handler mHandler;
-    private RequestEnvelopeOuterClass.RequestEnvelope.AuthInfo mAuthInfo;
-    private final OkHttpClient client;
-    private PokemonGo pokemonGo;
+    private AuthInfo mAuthInfo;
+    private NianticService mNianticService;
+    private final OkHttpClient mClient;
+    private final OkHttpClient mPoGoClient;
+    private PokemonGo mPokemonGo;
 
     public static NianticManager getInstance(){
         return instance;
     }
 
     private NianticManager(){
-        client = new OkHttpClient.Builder()
+        mPoGoClient = new OkHttpClient.Builder()
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .writeTimeout(10, TimeUnit.SECONDS)
                 .readTimeout(30, TimeUnit.SECONDS)
@@ -54,10 +77,143 @@ public class NianticManager {
         HandlerThread thread = new HandlerThread("Niantic Manager Thread");
         thread.start();
         mHandler = new Handler(thread.getLooper());
+
+                  /*
+		This is a temporary, in-memory cookie jar.
+		We don't require any persistence outside of the scope of the login,
+		so it being discarded is completely fine
+		*/
+        CookieJar tempJar = new CookieJar() {
+            private final HashMap<String, List<Cookie>> cookieStore = new HashMap<String, List<Cookie>>();
+
+            @Override
+            public void saveFromResponse(okhttp3.HttpUrl url, List<Cookie> cookies) {
+                cookieStore.put(url.host(), cookies);
+            }
+
+            @Override
+            public List<Cookie> loadForRequest(okhttp3.HttpUrl url) {
+                List<Cookie> cookies = cookieStore.get(url.host());
+                return cookies != null ? cookies : new ArrayList<Cookie>();
+            }
+        };
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        mClient = new OkHttpClient.Builder()
+                .cookieJar(tempJar)
+                .addInterceptor(new NetworkRequestLoggingInterceptor())
+                .build();
+
+        mNianticService = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(mClient)
+                .build()
+                .create(NianticService.class);
+    }
+
+    public void login(final String username, final String password, final LoginListener loginListener){
+        Callback<NianticService.LoginValues> valuesCallback = new Callback<NianticService.LoginValues>() {
+            @Override
+            public void onResponse(Call<NianticService.LoginValues> call, Response<NianticService.LoginValues> response) {
+                if(response.body() != null) {
+                    loginPTC(username, password, response.body(), loginListener);
+                }else{
+                    loginListener.authFailed("Fetching Pokemon Trainer Club's Login Url Values Failed");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<NianticService.LoginValues> call, Throwable t) {
+                loginListener.authFailed("Fetching Pokemon Trainer Club's Login Url Values Failed");
+            }
+        };
+        Call<NianticService.LoginValues> call = mNianticService.getLoginValues();
+        call.enqueue(valuesCallback);
+    }
+
+    private void loginPTC(final String username, final String password, NianticService.LoginValues values, final LoginListener loginListener){
+        HttpUrl url = HttpUrl.parse(LOGIN_URL).newBuilder()
+                .addQueryParameter("lt", values.getLt())
+                .addQueryParameter("execution", values.getExecution())
+                .addQueryParameter("_eventId", "submit")
+                .addQueryParameter("username", username)
+                .addQueryParameter("password", password)
+                .build();
+
+        OkHttpClient client = mClient.newBuilder()
+                .followRedirects(false)
+                .followSslRedirects(false)
+                .build();
+
+        NianticService service = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+                .create(NianticService.class);
+
+        Callback<NianticService.LoginResponse> loginCallback = new Callback<NianticService.LoginResponse>() {
+            @Override
+            public void onResponse(Call<NianticService.LoginResponse> call, Response<NianticService.LoginResponse> response) {
+                String location = response.headers().get("location");
+                String ticket = location.split("ticket=")[1];
+                requestToken(ticket, loginListener);
+            }
+
+            @Override
+            public void onFailure(Call<NianticService.LoginResponse> call, Throwable t) {
+                loginListener.authFailed("Pokemon Trainer Club Login Failed");
+            }
+        };
+        Call<NianticService.LoginResponse> call = service.login(url.toString());
+        call.enqueue(loginCallback);
+    }
+
+    private void requestToken(String code, final LoginListener loginListener){
+        Log.d(TAG, "requestToken() called with: code = [" + code + "]");
+        HttpUrl url = HttpUrl.parse(LOGIN_OAUTH).newBuilder()
+                .addQueryParameter("client_id", CLIENT_ID)
+                .addQueryParameter("redirect_uri", REDIRECT_URI)
+                .addQueryParameter("client_secret", CLIENT_SECRET)
+                .addQueryParameter("grant_type", "refresh_token")
+                .addQueryParameter("code", code)
+                .build();
+
+        Callback<ResponseBody> authCallback = new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    String token = response.body().string().split("token=")[1];
+                    token = token.split("&")[0];
+                    loginListener.authSuccessful(token);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    loginListener.authFailed("Pokemon Trainer Club Authentication Failed");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                t.printStackTrace();
+                loginListener.authFailed("Pokemon Trainer Club Authentication Failed");
+            }
+        };
+        Call<ResponseBody> call = mNianticService.requestToken(url.toString());
+        call.enqueue(authCallback);
+    }
+
+    public interface LoginListener {
+        void authSuccessful(String authToken);
+        void authFailed(String message);
     }
 
     /**
-     * Sets the google auth token for the auth info  also invokes the onLogin callback.
+     * Sets the google auth token for the auth info also invokes the onLogin callback.
      * @param token - a valid google auth token.
      */
     public void setGoogleAuthToken(@NonNull final String token) {
@@ -65,9 +221,30 @@ public class NianticManager {
             @Override
             public void run() {
                 try {
-                    mAuthInfo = new GoogleLogin(client).login(token);
-                    pokemonGo = new PokemonGo(mAuthInfo, client);
-                    EventBus.getDefault().post(new LoginEventResult(true, mAuthInfo, pokemonGo));
+                    mAuthInfo = new GoogleLogin(mPoGoClient).login(token);
+                    mPokemonGo = new PokemonGo(mAuthInfo, mPoGoClient);
+                    EventBus.getDefault().post(new LoginEventResult(true, mAuthInfo, mPokemonGo));
+                } catch (LoginFailedException e) {
+                    e.printStackTrace();
+                } catch (RemoteServerException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Sets the pokemon trainer club auth token for the auth info also invokes the onLogin callback.
+     * @param token - a valid pokemon trainer club auth token.
+     */
+    public void setPTCAuthToken(@NonNull final String token) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mAuthInfo = new PtcLogin(mPoGoClient).login(token);
+                    mPokemonGo = new PokemonGo(mAuthInfo, mPoGoClient);
+                    EventBus.getDefault().post(new LoginEventResult(true, mAuthInfo, mPokemonGo));
                 } catch (LoginFailedException e) {
                     e.printStackTrace();
                 } catch (RemoteServerException e) {
@@ -82,9 +259,9 @@ public class NianticManager {
             @Override
             public void run() {
                 try {
-                    mAuthInfo = new PtcLogin(client).login(username, password);
-                    pokemonGo = new PokemonGo(mAuthInfo, client);
-                    EventBus.getDefault().post(new LoginEventResult(true, mAuthInfo, pokemonGo));
+                    mAuthInfo = new PtcLogin(mPoGoClient).login(username, password);
+                    mPokemonGo = new PokemonGo(mAuthInfo, mPoGoClient);
+                    EventBus.getDefault().post(new LoginEventResult(true, mAuthInfo, mPokemonGo));
                 } catch (LoginFailedException e) {
                     EventBus.getDefault().post(new LoginEventResult(false, null, null));
                 } catch (RemoteServerException e) {
@@ -99,8 +276,8 @@ public class NianticManager {
             @Override
             public void run() {
                 try {
-                    pokemonGo.setLocation(lat, longitude, alt);
-                    EventBus.getDefault().post(new CatchablePokemonEvent(pokemonGo.getMap().getCatchablePokemon()));
+                    mPokemonGo.setLocation(lat, longitude, alt);
+                    EventBus.getDefault().post(new CatchablePokemonEvent(mPokemonGo.getMap().getCatchablePokemon()));
                 } catch (LoginFailedException e) {
                     EventBus.getDefault().post(new TokenExpiredEvent()); //Because we aren't coming from a log in event, the token must have expired.
                 } catch (RemoteServerException e) {
