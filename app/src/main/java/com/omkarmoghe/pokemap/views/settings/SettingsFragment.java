41c3a1113a155ac21a3fe954ba26a99255d185e1
Chris <chris.beda@gmail.com>
2016-07-26 18:30:52 -0700

    Fixed crash due to removal of setting username and password in app settings.

--- app/src/main/java/com/omkarmoghe/pokemap/views/settings/SettingsFragment.java
+++ app/src/main/java/com/omkarmoghe/pokemap/views/settings/SettingsFragment.java
@@ -22,24 +22,6 @@ public class SettingsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
-
-        final Preference usernamePref = findPreference(getString(R.string.pref_username_key));
-        usernamePref.setSummary(pref.getString(getString(R.string.pref_username_key), getString(R.string.pref_default_username)));
-
-        final Preference passwordPref = findPreference(getString(R.string.pref_password_key));
-        passwordPref.setSummary(pref.getString(getString(R.string.pref_password_key).replaceAll(".", "*"), getString(R.string.pref_default_password).replaceAll(".", "*")));
-
-        // Create change listener
-        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
-            @Override
-            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-                if(s.equals(getString(R.string.pref_username_key)))
-                    usernamePref.setSummary(pref.getString(getString(R.string.pref_username_key), getString(R.string.pref_default_username)));
-                else if(s.equals(getString(R.string.pref_password_key)))
-                    passwordPref.setSummary(pref.getString(getString(R.string.pref_password_key).replaceAll(".", "*"), getString(R.string.pref_default_password).replaceAll(".", "*")));
-            }
-        };
-
         // Create Theme button to link to Theme Fragment
         Preference button = (Preference) getPreferenceManager().findPreference(getString(R.string.pref_theme_button_key));
         if (button != null) {
